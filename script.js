9149b2254236910f2cd750bd9bea58b314086af6
Aadityab2003 <aadityabaraskar2003@gmail.com>
2025-09-15 19:39:35 +0530

    Gui Process

---
+++ script.js
@@ -0,0 +1,30 @@
+const questions = [
+    {
+        question: "What is the capital of India?",
+        answers:[
+            { text: "New Delhi", correct: true },
+            { text: "Mumbai", correct: false },
+            { text: "Chh.Sambhajinagar", correct: false },
+            { text: "Banglore", correct: false }
+        ]
+
+    },
+    {
+        question: "What is the capital of Maharashtra?",
+        answers:[
+            { text: "Chh.Sambhajinagar", correct: false },
+            { text: "Mumbai", correct: true },
+            { text: "Pune", correct: false },
+            { text: "Nagpur", correct: false }
+        ]
+    },
+    {
+         question: "What is the Largest Animal in the world?",
+        answers:[
+            { text: "Dog", correct: false },
+            { text: "Elephant", correct: false },
+            { text: "Blue Whale", correct: true },
+            { text: "Camel", correct: false }
+        ]
+    }
+]
